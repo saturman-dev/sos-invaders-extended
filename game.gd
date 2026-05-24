39d6b64df23c8a4f7a2333aa5740e162c982b336
saturman-dev <vania999900@gmail.com>
2026-05-24 12:45:28 +0400

    0.0.3
    tried windowing

--- game.gd
+++ game.gd
@@ -123,5 +123,14 @@ func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		if Globals.game_running == true:
 			$pauseC.add_child(pause.instantiate())
+	if event.is_action_pressed("fullscreen"):
+		toggle_fullscreen()
 	if event.is_action_pressed("ui_accept"):
 		pass
+
+func toggle_fullscreen():
+	var current = DisplayServer.window_get_mode()
+	if current == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
